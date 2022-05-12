8f2a8a8aeeedb008e353875d00ff89dd095e7615
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-05-12 23:40:53 +0200

    Update Helper.cs
    - Move BitmapToImageSource function to Code Snippets.

--- Helper.cs
+++ Helper.cs
@@ -80,21 +80,6 @@ internal static class Helper {
             return number == 1;
         }
 
-        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
-        {
-            using (MemoryStream memory = new MemoryStream()) {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
-
-                return bitmapimage;
-            }
-        }
-
     }
 
     /// <summary>
