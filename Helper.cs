024ad10e6d847afdd4ae53e4c64f16350963462b
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-05-12 22:14:58 +0200

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,460 @@
+// Clonk's Helper Class
+// Last updated: 5/12/2022
+
+#region Imports
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.VisualBasic;
+#endregion
+
+#region Public Enums
+public enum FileSizes {
+    Byte,
+    Kilobyte,
+    Megabyte,
+    Gigabyte,
+    Terabyte,
+    Petabyte,
+    Exabyte,
+}
+#endregion
+
+#region Public Structs
+public struct FileSize {
+    #region Properties
+    public FileSizes FileSizes { get; private set; }
+    public double Size { get; private set; }
+    #endregion
+
+    #region Constructor
+    internal FileSize(double fileSize, FileSizes fileSizes)
+    {
+        FileSizes = fileSizes;
+        Size = fileSize;
+    }
+    #endregion
+}
+public struct AResult {
+
+    #region Properties
+    public Exception Exception { get; private set; }
+    public object Result { get; private set; }
+    #endregion
+
+    #region Constructor
+    public AResult(Exception ex, object result)
+    {
+        Exception = ex;
+        Result = result;
+    }
+    #endregion
+
+}
+#endregion
+
+internal static class Helper {
+
+    #region Classes
+    /// <summary>
+    /// Convert stuff.
+    /// </summary>
+    public class ConverterStuff {
+
+        public static bool ConvertIntToBool(int number)
+        {
+            return number == 1;
+        }
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream()) {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// If you override the <see cref="Console.Out"/> property with a new instance of the <see cref="ConsoleOutputRedirector"/>
+    /// you can subscribe to its <see cref="ConsoleOutputRedirector.OnLineAdded"/> event to be notified when a new line got written to the console using <see cref="Console.WriteLine(string)"/>.
+    /// <br/><br/>
+    /// You also have the possibility to view the history of the console using <see cref="ConsoleOutputRedirector.GetLines()"/>.
+    /// </summary>
+    public class ConsoleOutputRedirector : TextWriter {
+
+        #region Variables
+        private List<string> lines;
+        private TextWriter original;
+        #endregion
+
+        #region Events
+        public delegate void LineAdded(string value);
+        public event LineAdded OnLineAdded;
+        #endregion
+
+        #region Constructor
+        public ConsoleOutputRedirector(TextWriter original)
+        {
+            lines = new List<string>();
+            this.original = original;
+        }
+        #endregion
+
+        #region Overrides
+        public override Encoding Encoding
+        {
+            get { return Encoding.Default; }
+        }
+        public override void WriteLine(string value)
+        {
+            lines.Add(value);
+            original.WriteLine(value);
+            OnLineAdded?.Invoke(value);
+        }
+        // You need to override other methods also
+        #endregion
+
+        #region Functions
+        public string[] GetLines()
+        {
+            return lines.ToArray();
+        }
+        #endregion
+
+    }
+
+    /// <summary>
+    /// A basic ini reader/writer using kernel32 dll imports.
+    /// </summary>
+    public class INIController {
+
+        #region DllImports
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileStringA", CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int GetPrivateProfileString([MarshalAs(UnmanagedType.VBByRefStr)] ref string lpApplicationName, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpKeyName, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpDefault, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpReturnedString, int nSize, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpFileName);
+
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileStringA", CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int WritePrivateProfileString([MarshalAs(UnmanagedType.VBByRefStr)] ref string lpApplicationName, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpKeyName, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpString, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpFileName);
+        #endregion
+
+        #region Functions
+        public static string ReadValueFromFile(string strSection, string strKey, string strDefault, string strFile)
+        {
+            string lpReturnedString = Strings.Space(1024);
+            int privateProfileString = GetPrivateProfileString(ref strSection, ref strKey, ref strDefault, ref lpReturnedString, lpReturnedString.Length, ref strFile);
+            return lpReturnedString.Substring(0, privateProfileString);
+        }
+        public static bool WriteValueToFile(string strSection, string strKey, string strValue, string strFile)
+        {
+            return WritePrivateProfileString(ref strSection, ref strKey, ref strValue, ref strFile) != 0;
+        }
+        #endregion
+
+    }
+
+    /// <summary>
+    /// String Compression stuff.
+    /// </summary>
+    public class StringCompression {
+        /// <summary>
+        /// Compresses a string and returns a deflate compressed, Base64 encoded string.
+        /// </summary>
+        /// <param name="uncompressedString">String to compress</param>
+        public static string Compress(string uncompressedString, string returnStringOnError = "")
+        {
+            try {
+                byte[] compressedBytes;
+
+                using (var uncompressedStream = new MemoryStream(Encoding.UTF8.GetBytes(uncompressedString))) {
+                    using (var compressedStream = new MemoryStream()) {
+                        using (var compressorStream = new DeflateStream(compressedStream, CompressionLevel.Optimal, true)) {
+                            uncompressedStream.CopyTo(compressorStream);
+                        }
+                        compressedBytes = compressedStream.ToArray();
+                    }
+                }
+
+                return Convert.ToBase64String(compressedBytes);
+            }
+            catch (Exception) {
+                return returnStringOnError;
+            }
+        }
+
+        /// <summary>
+        /// Decompresses a deflate compressed, Base64 encoded string and returns an uncompressed string.
+        /// </summary>
+        /// <param name="compressedString">String to decompress.</param>
+        public static string Decompress(string compressedString, string returnStringOnError = "")
+        {
+            try {
+                byte[] decompressedBytes;
+
+                var compressedStream = new MemoryStream(Convert.FromBase64String(compressedString));
+
+                using (var decompressorStream = new DeflateStream(compressedStream, CompressionMode.Decompress)) {
+                    using (var decompressedStream = new MemoryStream()) {
+                        decompressorStream.CopyTo(decompressedStream);
+
+                        decompressedBytes = decompressedStream.ToArray();
+                    }
+                }
+
+                return Encoding.UTF8.GetString(decompressedBytes);
+            }
+            catch (Exception) {
+                return returnStringOnError;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Parsing stuff.
+    /// </summary>
+    public class ParseExtension {
+        public static bool Parse(string s, bool defaultValue = false)
+        {
+            bool result;
+            bool parseResult = bool.TryParse(s, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static int Parse(string s, int defaultValue = 0)
+        {
+            int result;
+            bool parseResult = int.TryParse(s, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static ushort Parse(string s, ushort defaultValue = 0)
+        {
+            ushort result;
+            bool parseResult = ushort.TryParse(s, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static ulong Parse(string s, ulong defaultValue = 0)
+        {
+            ulong result;
+            bool parseResult = ulong.TryParse(s, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static double Parse(string s, double defaultValue = 0)
+        {
+            double result;
+            bool parseResult = double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static float Parse(string s, float defaultValue = 0f)
+        {
+            float result;
+            bool parseResult = float.TryParse(s, out result);
+            if (parseResult) {
+                return result;
+            }
+            return defaultValue;
+        }
+    }
+
+    /// <summary>
+    /// Windows Presentation Foundation (WPF) stuff.
+    /// <para>Updated: <b>5/12/2022</b></para>
+    /// </summary>
+    public class WPFStuff {
+
+        #region Method
+        public static void GetLogicalChildCollection<T>(DependencyObject parent, List<T> logicalCollection) where T : DependencyObject
+        {
+            IEnumerable children = LogicalTreeHelper.GetChildren(parent);
+            foreach (object child in children) {
+                if (child is DependencyObject) {
+                    DependencyObject depChild = child as DependencyObject;
+                    if (child is T) {
+                        logicalCollection.Add(child as T);
+                    }
+                    GetLogicalChildCollection(depChild, logicalCollection);
+                }
+            }
+        }
+        #endregion
+
+        #region Functions
+        public static List<T> GetLogicalChildCollection<T>(DependencyObject parent) where T : DependencyObject
+        {
+            List<T> logicalCollection = new List<T>();
+            GetLogicalChildCollection(parent, logicalCollection);
+            return logicalCollection;
+        }
+        public static bool IsWindowOpen<T>(string name = "") where T : Window
+        {
+            return string.IsNullOrEmpty(name)
+               ? Application.Current.Windows.OfType<T>().Any()
+               : Application.Current.Windows.OfType<T>().Any(w => w.Name.Equals(name));
+        }
+        public static Window GetOpenedWindow<T>(string name = "") where T : Window
+        {
+            return string.IsNullOrEmpty(name)
+               ? Application.Current.Windows.OfType<T>().FirstOrDefault()
+               : Application.Current.Windows.OfType<T>().FirstOrDefault(w => w.Name.Equals(name));
+        }
+        #endregion
+
+    }
+    #endregion
+
+    #region Functions
+    public static byte[] GetByteArray(Stream input)
+    {
+        using (MemoryStream ms = new MemoryStream()) {
+            input.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+
+    public static bool IsUserConnectedToTheInternet()
+    {
+        try {
+            using (Ping ping = new Ping()) {
+                PingReply reply = ping.Send("8.8.8.8");
+                if (reply != null && reply.Status == IPStatus.Success) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        catch (Exception) {
+            return false;
+        }
+    }
+    public static string GetFileVersion(string fileName)
+    {
+        try {
+            string raw = FileVersionInfo.GetVersionInfo(fileName).FileVersion;
+            if (!string.IsNullOrEmpty(raw)) {
+                if (raw.Contains(",")) {
+                    string raw2 = raw.Replace(',', '.');
+                    if (raw2.Contains(" ")) {
+                        return raw2.Replace(" ", "");
+                    }
+                    return raw2;
+                }
+                return raw;
+            }
+            return string.Empty;
+        }
+        catch (Exception) {
+            return string.Empty;
+        }
+    }
+
+    public static AResult GetMD5StringFromFolder(string folder)
+    {
+        try {
+            List<string> files = Directory.GetFiles(folder, "*.*", SearchOption.TopDirectoryOnly).OrderBy(p => p).ToList();
+            using (MD5 md5 = MD5.Create()) {
+
+                // Generate hash from all files in directory
+                for (int i = 0; i < files.Count; i++) {
+                    string file = files[i];
+
+                    if (Path.GetFileName(file).ToLower() == "installscript.vdf")
+                        continue;
+
+                    // Hash path
+                    string realtivePath = file.Substring(folder.Length + 1);
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(realtivePath.ToLower());
+                    md5.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // Hash contents
+                    byte[] contentBytes = File.ReadAllBytes(file);
+                    if (contentBytes == null) return new AResult(new ArgumentNullException("contentBytes was null."), null);
+
+                    if (i == (files.Count - 1)) {
+                        md5.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+                    }
+                    else {
+                        md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                    }
+
+                }
+
+                return new AResult(null, BitConverter.ToString(md5.Hash).Replace("-", "").ToLower());
+            }
+        }
+        catch (Exception ex) {
+            return new AResult(ex, null);
+        }
+    }
+
+    public static double GetVRAM()
+    {
+        try {
+            using (ManagementClass c = new ManagementClass("Win32_VideoController")) {
+                foreach (ManagementObject o in c.GetInstances()) {
+                    FileSize size = GetExactFileSize(long.Parse(o["AdapterRam"].ToString()));
+                    return size.Size;
+                }
+            }
+        }
+        catch (Exception) { }
+        return 1024;
+    }
+
+    public static FileSize GetExactFileSize(long byteCount)
+    {
+        if (byteCount == 0)
+            return new FileSize(0, FileSizes.Byte);
+
+        long bytes = Math.Abs(byteCount);
+        int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+        double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+        return new FileSize(Math.Sign(byteCount) * num, (FileSizes)place);
+    }
+    public static string GetExactFileSize2(long byteCount)
+    {
+        string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; // Longs run out around EB
+        if (byteCount == 0)
+            return "0" + suf[0];
+        long bytes = Math.Abs(byteCount);
+        int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+        double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+        return string.Format("{0} {1}", (Math.Sign(byteCount) * num).ToString(), suf[place]);
+    }
+    #endregion
+
+}
