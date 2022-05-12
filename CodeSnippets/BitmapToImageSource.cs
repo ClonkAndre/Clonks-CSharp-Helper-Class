f40d9f0f269826036ad3f2d728a0e1acef01f96d
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-05-12 23:41:16 +0200

    Add files via upload

---
+++ CodeSnippets/BitmapToImageSource.cs
@@ -0,0 +1,21 @@
+/// <summary>
+/// Convert stuff.
+/// </summary>
+public class ConverterStuff {
+
+    public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+    {
+        using (MemoryStream memory = new MemoryStream()) {
+            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            BitmapImage bitmapimage = new BitmapImage();
+            bitmapimage.BeginInit();
+            bitmapimage.StreamSource = memory;
+            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapimage.EndInit();
+
+            return bitmapimage;
+        }
+    }
+
+}
