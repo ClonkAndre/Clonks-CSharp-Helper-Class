8ec83805606b4a520e4de3a5e678bed6efc50c68
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-07-21 21:43:35 +0200

    Added WindowSinker class

--- CodeSnippets/WPFStuff.cs
+++ CodeSnippets/WPFStuff.cs
@@ -1,6 +1,6 @@
 /// <summary>
 /// Windows Presentation Foundation (WPF) stuff.
-/// <para>Updated: <b>5/12/2022</b></para>
+/// <para>Updated: <b>7/21/2022</b></para>
 /// </summary>
 public class WPFStuff {
 
@@ -40,5 +40,95 @@ public class WPFStuff {
            : Application.Current.Windows.OfType<T>().FirstOrDefault(w => w.Name.Equals(name));
     }
     #endregion
+			
+		#region Classes
 
+    /// <summary>
+    /// Class that allows a WPF <see cref="System.Windows.Window"/> to be on top of every opened Window. Even games that run in fullscreen mode.
+    /// </summary>
+    public class WindowSinker {
+
+        #region Variables
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 SWP_NOACTIVATE = 0x0010;
+        private const UInt32 SWP_NOZORDER = 0x0004;
+        private const int WM_ACTIVATEAPP = 0x001C;
+        private const int WM_ACTIVATE = 0x0006;
+        private const int WM_SETFOCUS = 0x0007;
+        private const int WM_WINDOWPOSCHANGING = 0x0046;
+
+        private static readonly IntPtr HWND_BOTTOM = new IntPtr(0);
+
+        private Window Window = null;
+        #endregion
+
+        #region Constructor
+        public WindowSinker(Window Window)
+        {
+            this.Window = Window;
+        }
+        #endregion
+
+        #region Methods
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr DeferWindowPos(IntPtr hWinPosInfo, IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr BeginDeferWindowPos(int nNumWindows);
+
+        [DllImport("user32.dll")]
+        static extern bool EndDeferWindowPos(IntPtr hWinPosInfo);
+
+        void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var Handle = (new WindowInteropHelper(Window)).Handle;
+
+            var Source = HwndSource.FromHwnd(Handle);
+            Source.RemoveHook(new HwndSourceHook(WndProc));
+        }
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var Hwnd = new WindowInteropHelper(Window).Handle;
+            SetWindowPos(Hwnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+
+            var Handle = (new WindowInteropHelper(Window)).Handle;
+
+            var Source = HwndSource.FromHwnd(Handle);
+            Source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        IntPtr WndProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_SETFOCUS) {
+                hWnd = new WindowInteropHelper(Window).Handle;
+                SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        public void Sink()
+        {
+            Window.Loaded += OnLoaded;
+            Window.Closing += OnClosing;
+        }
+
+        public void Unsink()
+        {
+            Window.Loaded -= OnLoaded;
+            Window.Closing -= OnClosing;
+        }
+
+        #endregion
+
+    }
+
+    #endregion
+			
 }
